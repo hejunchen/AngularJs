8d5b4b1d6ceb6c7e0af5de56d3df56c69610e6af
hejunchen@gmail.com <hejunchen@gmail.com>
2014-12-29 16:41:31 -0800

    complete the submit function

--- Views/AngularJSApplication/myNoteApp.js
+++
@@ -1,4 +0,0 @@
-/**
- * Created by hchen on 10/10/2014.
- */
-var app = angular.module("myNoteApp", []);
--- Views/AngularJSApplication/myNoteCtrl.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by hchen on 10/10/2014.
- */
-app.controller("myNoteCtrl", function($scope) {
-    $scope.message = "";
-    $scope.left  = function() {return 100 - $scope.message.length;};
-    $scope.clear = function() {$scope.message = "";};
-    $scope.save  = function() {alert("Note Saved");};
-});
